f255647e161ca5d9d0eda23e8cc02ac76a12316c
Shino <Shino@SHINO>
2023-09-24 19:06:59 -0700

    Agregar archivos de proyecto.

---
+++ PracticasParaEstructurada/Clases/Metodos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PracticasParaEstructurada.Clases
+{
+    public class Metodos
+    {
+        public int SumaRecursiva(int numeroIngresado, int contador, int resultadoSuma)
+        {
+            if (numeroIngresado < 0)
+            {
+                return resultadoSuma;
+            }
+            else if(numeroIngresado == contador)
+            {
+                return resultadoSuma;
+            }
+            else
+            {
+                contador++;
+                resultadoSuma += contador;
+                return SumaRecursiva(numeroIngresado, contador, resultadoSuma);
+            }
+        }
+    }
+    public class Persona
+    {
+        public int edad { get; set; }
+        public string nombrePersona { get; set; }
+        public string resultado { get; set; }
+
+        public string DatosPerosna(int edad, string nombrePersona)
+        {
+            resultado = $"{nombrePersona}, con edad de {edad}";
+            return resultado;
+        }
+
+    }
+}
---
+++ PracticasParaEstructurada/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using PracticasParaEstructurada.Clases;
+
+namespace PracticasParaEstructurada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("ingresa tu nombre y tu edad: ");
+            Persona persona = new Persona();
+            persona.nombrePersona = Console.ReadLine();
+            persona.edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(persona.DatosPerosna(persona.edad,persona.nombrePersona));
+            Console.ReadKey();
+        }
+    }
+}
